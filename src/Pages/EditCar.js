ecbc3a49dbd5ce8942ce12b113d4a7f673d94e6e
Mandragid <mandragid@MacBook-Pro-Macbook.local>
2022-12-22 23:15:06 +0700

    add edit feat

--- src/Pages/EditCar.js
+++ src/Pages/EditCar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import Navigation from "../Components/Navigation";
 
@@ -10,6 +10,7 @@ const EditCar = () => {
   const [price, setPrice] = useState("");
   const [carData, setCarData] = useState({});
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -54,20 +55,54 @@ const EditCar = () => {
       });
   };
 
+  const handleEdit = () => {
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: {
+        access_token: token,
+      },
+    };
+
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("name", name);
+    formData.append("category", category);
+    formData.append("price", price);
+    formData.append("status", false);
+
+    axios
+      .put(
+        `https://bootcamp-rent-cars.herokuapp.com/admin/car/${id}`,
+        formData,
+        config
+      )
+      .then((res) => {
+        console.log("edit berhasil");
+        navigate("/discovery");
+      })
+      .catch((err) => {
+        console.log("gagal edit");
+      });
+  };
+
   return (
     <div>
       <Navigation />
 
       {Object.entries(carData).length ? (
         <div>
-          <input placeholder={carData.name} />
-          <input placeholder={carData.category} />
-          <input placeholder={carData.price} />
-          <input type={"file"} placeholder={carData.image} />
+          <input onChange={handleName} defaultValue={carData.name} />
+          <input onChange={handleCategory} defaultValue={carData.category} />
+          <input onChange={handlePrice} defaultValue={carData.price} />
+          <input
+            onChange={handleImage}
+            type={"file"}
+            placeholder={carData.image}
+          />
           <button>
             <Link to="/discovery">Cancel</Link>
           </button>
-          <button>Save</button>
+          <button onClick={handleEdit}>Save</button>
         </div>
       ) : null}
     </div>
